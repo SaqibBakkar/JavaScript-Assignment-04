68bfd28e9a8f069f801b3101df52f7278422e6c5
SaqibBakkar <sbakkar301@gmail.com>
2025-02-24 00:10:29 -0800

    Add files via upload

---
+++ Assignment 04/app.js
@@ -0,0 +1,162 @@
+
+// Home Assignment
+
+// Chapter 15 (Array I)
+
+// Q1)
+// var arr = []    ("empty")
+
+// Q2)
+// var arr = ["Hellow User"];
+// console.log(arr);
+
+// Q3)
+// var alphabet = ["h", "i", "j", "k"];
+// alert(alphabet[2]);
+
+// Q4)
+// var alphabet = ["h", "i", "j", "k", "l", "m", "n", "o"];
+// alert(alphabet.length);
+
+// Q5)
+// var cityies = ["Karachi", "Lahore"];
+// cityies[2] = "Multan";
+// alert(cityies);
+
+// Chapter 16 (Array II)
+
+// Q1)
+// var names = ["Ali", "Hamza", "Mavia"];
+// names.push("Rayyan");
+// alert(names[3]);
+
+// Q2)
+// var Alphabet=["h","i","j","k"];
+// Alphabet.pop(3);
+// console.log(Alphabet);
+
+// Q3)
+// var Alphabet=["h","i","j","k"];
+// Alphabet.push(12);
+// console.log(Alphabet);
+
+// Chapter 16 (Array III)
+
+// Q1)
+// var sizes = ["S", "M", "XL", 'XXL', "XXXL"];
+// sizes.shift(0);
+// console.log(sizes);
+
+// Q2)
+// var sizes = ["S", "M", "XL", 'XXL', "XXXL"];
+// sizes.push(10);
+// sizes.push(18);
+// sizes.push(9);
+// console.log(sizes);
+
+// Q3)
+// var cityies = ["Karachi"];
+// cityies[1] = "Lahore";
+// alert(cityies[0]);
+// console.log(cityies);
+
+// Q4)
+// var sizes = ["S", "M", "XL", 'XXL', "XXXL"];
+// sizes.splice(2, 0, "L");
+// console.log(sizes);
+
+// Q5)
+// var sizes = ["S", "M", "XL", 'XXL', "XXXL"];
+// var regSizes = sizes.slice(0, 3);
+// console.log(regSizes);
+
+// Q6)
+// var pets = ["dog", "cat", "ox", "duck", "frog"];
+// pets.splice(1, 2, "fish", "rabbit");
+// console.log(pets); 
+
+// Q7)
+// var pets = ["dog", "cat", "ox", "duck", "frog"];
+// pets.splice(1, 2);
+// console.log(pets); 
+
+// Q8)
+// var pets = ["dog", "cat", "ox", "duck", "frog"];
+// var reducedPets = pets.slice(3, 5);
+// console.log(reducedPets); 
+
+
+// Class Assignment
+
+// Chapter 15 (Array)
+
+// Q1)
+// var studentNames = [];
+// studentNames.push("Muhammad");
+// studentNames.push("Saqib");
+// console.log(studentNames);
+
+// Q2)
+// var studentNames = [];
+// studentNames.push("Muhammad Saqib");
+// studentNames.push("Muhammad Sajid");
+// console.log(studentNames);
+
+// Q3)
+// var spring = ["Hellow User"];
+// console.log(spring);
+
+// Q4)
+// var number = [10];
+// console.log(number);
+
+// Q5)
+// var boolean = [true];
+// console.log(boolean);
+
+// Q6)
+// var mixed = [42, "hello", True, 3, 14];
+// console.log(mixed);
+
+// Q7)
+// var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+// console.log(qualifications);
+// document.write ("<h2>Qualifications </h2>");
+// document.write(qualifications);
+
+// Q8)
+
+
+// Q9)
+
+
+// Q10)
+
+
+// Q11)
+// var cities = ["Karachi", "Lahore", "Multan", "Islamabad", "Quetta", "Peshawar"]
+// var selected = cities.slice(3, 5);
+// document.write("Cities List: <br>", cities);
+// document.write("<br> <br> selected Cities List: <br>", selected);
+
+// Q12)
+// var arr = ["This ", " is ", " my ", " cat"];
+// document.write("Array: <br>", arr);
+// document.write("<br> Spring: <br>","This ", " is ", " my ", " cat");
+
+// Q13)
+// var device = ["Keyboard", "Mouse", "Printer", "Moniter"]
+// console.log("Devices: ",device);
+
+// var result_1 = device.pop();
+// var result_2 = device.pop();
+// var result_3 = device.pop();
+// var result_4 = device.pop();
+
+// console.log(" Out: ",result_1);
+// console.log(" Out: ",result_2);
+// console.log(" Out: ",result_3);
+// console.log(" Out: ",result_4);
+
+// Q14)
+
